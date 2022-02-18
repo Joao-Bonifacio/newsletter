381b989ad292cb38a14cc0d88b38676ba3bb04ba
Joao-Bonifacio <joaojunior.b22@gmail.com>
2022-02-18 18:07:02 -0300

    html

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.engine('ejs',require('ejs').renderFile)
-app.set('view engine','ejs')
+app.engine('html',require('ejs').renderFile)
+app.set('view engine','html')
 app.set('views',path.join(__dirname,'/views'))
 //app.use('/static', express.static(__dirname + '/public'))
 app.use('/public',express.static('public'))
@@ -12,10 +12,9 @@ app.use(express.urlencoded({ extended: false}))
 
 //const tst = require('./script.js') //importando módulo
 
-
 app.get('/', (req,res) => {
     if (req.query.seach == null) {
-        res.render('index',{tst})
+        res.render('index',{})
     }else{
         res.send('Você pesquisou: '+req.query.search) //futuramente buscar no banco de dados
     }
